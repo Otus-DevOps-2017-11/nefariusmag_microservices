50d5c3ab27c26107596e2b2da52b92de46623fe2
Dmitriy Erokhin <nefariusmag@gmail.com>
2018-02-12 15:16:59 +0300

    Add test for reddit

---
+++ reddit/simpletest.rb
@@ -0,0 +1,14 @@
+require_relative './app'
+require 'test/unit'
+require 'rack/test'
+set :environment, :test
+class MyAppTest < Test::Unit::TestCase
+  include Rack::Test::Methods
+  def app
+    Sinatra::Application
+  end
+  def test_get_request
+    get '/'
+    assert last_response.ok?
+  end
+end
